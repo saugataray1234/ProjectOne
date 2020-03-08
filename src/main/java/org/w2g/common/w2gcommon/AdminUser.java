f710f268e30715e4e98141f955742e0b57309f77
unknown <saugataray1234@gmail.com>
2020-03-08 20:24:12 +0530

    Our First Commit

---
+++ src/main/java/org/w2g/common/w2gcommon/AdminUser.java
@@ -0,0 +1,5 @@
+package org.w2g.common.w2gcommon;
+
+public class AdminUser extends User {
+
+}
---
+++ src/main/java/org/w2g/common/w2gcommon/ApplicationGlobal.java
@@ -0,0 +1,22 @@
+package org.w2g.common.w2gcommon;
+
+public class ApplicationGlobal {
+ public static final String CHAT_ROOM = "CHATROOM_";
+ public static final String SUBSCRIBLE_CHANNEL = "subscribleChannel_";
+ public static final String CONSUMER_CHANNEL = "consumer";
+ public static final String TEXT_TYPE_EVENT_START = "1";
+ public static final String TEXT_TYPE_EVENT_STOP = "0";
+ public static final String SHOW_ME_ONLINE_CHANNEL = "showMeOnlineChannel";
+ public static final String SHOW_ME_ONLINE_GC_CHANNEL = "showMeOnlineGCChannel";
+ public static final String WS_CREATED_EVENT_FOR_GC = "wsCreatedEventForGC";
+ public static final String CREATE_CHAT_ROOM_REQUEST = "createChatRoomRequest";
+ public static final String CHAT_REQUEST_CHANNEL = "chatRequestChannel";
+ public static final String CHAT_GENERAL_CONSUMER_CHANNEL = "chatGeneralConsumerChannel";
+ public static final String REPLY_TO_PRODUCER_CHANNEL = "replyToProducerChannel_";
+ public static final String PRODUCER_TO_GENERAL_CONSUMER_HEARTBEAT_CHANNEL = "ptghbc";
+ public static final String GENERAL_CONSUMER_TO_CONSUMER_SEND_BY_GENERAL_HEARTBEAT_CHANNEL = "gctcsbg";
+ public static final String ADMIN_USER_DISCONNECTED = "ADMIN_USER_DISCONNECTED";
+ public static final String LATEST_GC_USERS = "LATEST_GC_USERS";
+ public static final int HEARTBEAT_NUMBER_FOR_BEAN_CREATION = -1;
+ public static final String AVAILABLE_USERS = "AVAILABLE_USERS";
+}
